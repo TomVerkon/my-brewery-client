69d72f06151de0025fb00895275f906b7af23f98
Tom Verkon <tverkon@usa.net>
2019-08-08 15:26:02 -0400

    Initial Commit

---
+++ src/main/java/com/digits/mybreweryclient/MyBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.digits.mybreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digits/mybreweryclient/MyBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digits.mybreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
