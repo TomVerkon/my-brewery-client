330c8509c9e605f3ae997b76d2371df15079124a
Tom Verkon <tverkon@usa.net>
2019-08-12 16:32:37 -0400

    Completing Issue #5

--- src/main/java/com/digits/mybreweryclient/web/model/BeerDto.java
+++ src/main/java/com/digits/mybreweryclient/web/model/BeerDto.java
@@ -4,6 +4,11 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +20,32 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDto {
 
+    @Null(message = "id must be null")
     private UUID id;
+    
+    @NotBlank(message = "The beerName can not be blank")
     private String beerName;
+    
+    @NotNull(message = "You must assign a beer style")
     private BeerStyleEnum beerStyle;
+    
+    @NotNull
+    @Positive(message = "upc must be greater than 0")
     private Long upc;
+    
     private Integer quantityOnHand;
+
+    @NotNull
+    @Positive(message = "price must be greater than 0")
     private BigDecimal price;
+
+    @Null(message = "version must be null")
     private Integer version;
+
+    @Null(message = "createDate must be null")
     private OffsetDateTime createdDate;
+
+    @Null(message = "lastModifiedDate must be null")
     private OffsetDateTime lastModifiedDate;
+
 }
--- src/main/java/com/digits/mybreweryclient/web/model/CustomerDto.java
+++ src/main/java/com/digits/mybreweryclient/web/model/CustomerDto.java
@@ -3,6 +3,10 @@ package com.digits.mybreweryclient.web.model;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +18,19 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CustomerDto {
 
+    @Null
     private UUID id;
+    
+    @NotBlank
+    @Size(min = 3, max = 100)
     private String customerName;
+    
+    @Null(message = "version must be null")
     private Integer version;
+
+    @Null(message = "createDate must be null")
     private OffsetDateTime createdDate;
+
+    @Null(message = "lastModifiedDate must be null")
     private OffsetDateTime lastModifiedDate;
 }
